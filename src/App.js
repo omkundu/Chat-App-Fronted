a62aefd4ddfc8e6f30f658d61ff07268a6e60fad
omkundu <omkundu156@gmail.com>
2023-08-17 16:58:03 +0530

    chat-app-fronted-init

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { Box, TextField } from "@mui/material";
+import Button from "@mui/material/Button";
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+function App() {
+  const [socket, setSocket] = useState(null);
+  const [message, setMessage] = useState("");
+
+
+  useEffect(() => {
+    setSocket(io("http://localhost:4000"));
+  }, []);
+
+  function handleForm(e) {
+    e.preventDefault();
+    socket.emit("send-message",{message});
+    setMessage("")
+  }
+  return (
+    <div>
+      <Box component="form" onSubmit={handleForm}>
+        <TextField
+          size="small"
+          label="Write your message"
+          variant="standard"
+          value={message}
+          onChange={(e)=>setMessage(e.target.value)}
+        />
+        <Button variant="text" type="submit">
+          Send
+        </Button>
+      </Box>
+    </div>
+  );
+}
+
+export default App;
