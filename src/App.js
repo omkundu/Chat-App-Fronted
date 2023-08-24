5de1ac418f906026e864d4032a7c2db20660e69f
omkundu <omkundu156@gmail.com>
2023-08-24 10:59:32 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,65 @@
-import { Box, TextField } from "@mui/material";
+import {
+  Box,
+  Container,
+  IconButton,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@mui/material";
 import Button from "@mui/material/Button";
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import SendIcon from "@mui/icons-material/Send";
 function App() {
   const [socket, setSocket] = useState(null);
   const [message, setMessage] = useState("");
-
+  const [chat, setChat] = useState([]);
 
   useEffect(() => {
     setSocket(io("http://localhost:4000"));
-   
   }, []);
 
   useEffect(() => {
-    if(!socket) return;
-      socket.on("message-from-server",(data)=>{
-       console.log("messaged recieved",data)
-      });
+    if (!socket) return;
+    socket.on("message-from-server", (data) => {
+      //  console.log("messaged recieved",data)
+      setChat((prev) => [...prev, data.message]);
+    });
   }, [socket]);
 
   function handleForm(e) {
     e.preventDefault();
-    socket.emit("send-message",{message});
-    setMessage("")
+    socket.emit("send-message", { message });
+    setMessage("");
   }
   return (
     <div>
-      <Box component="form" onSubmit={handleForm}>
-        <TextField
+      <Container>
+        <Box sx={{ marginBottom: 5 }}>
+          {chat.map((message) => (
+            <Typography key={message}>{message}</Typography>
+          ))}
+        </Box>
+        <Box component="form" onSubmit={handleForm}>
+          <OutlinedInput 
+          variant={message}
+          placeholder="write your message"
           size="small"
-          label="Write your message"
-          variant="standard"
-          value={message}
-          onChange={(e)=>setMessage(e.target.value)}
-        />
-        <Button variant="text" type="submit">
-          Send
-        </Button>
-      </Box>
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton type="submit" edge="end">
+                  <SendIcon />
+                </IconButton>
+              </InputAdornment>
+            }
+   
+          />
+      
+        </Box>
+      </Container>
     </div>
   );
 }
